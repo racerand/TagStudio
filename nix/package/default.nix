1d1808351becdbbfc990d7a3343dcac0c8653959
Andreas Heglingegaard <16125637+racerand@users.noreply.github.com>
2026-05-14 13:35:26 +0200

    fix(nix): relax dependency on rawpy * fix(nix): ignore test_github_api_unavailable that seems to be incompatible with nix test env

--- nix/package/default.nix
+++ nix/package/default.nix
@@ -88,6 +88,7 @@ python3Packages.buildPythonApplication {
     "semver"
     "structlog"
     "typing-extensions"
+    "rawpy"
   ];
   pythonImportsCheck = [ "tagstudio" ];
 
@@ -132,6 +133,7 @@ python3Packages.buildPythonApplication {
     "test_json_migration"
     "test_library_migrations"
     "test_update_tags"
+    "test_github_api_unavailable" # Look into fatal python error
   ];
   disabledTestPaths = [
     "tests/qt/test_build_tag_panel.py"
